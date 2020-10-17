fa9b3d9b9853eee2b0d2ef78db5622156ae7a5cc
florvimberg <florvimberg@gmail.com>
2020-10-17 20:46:11 -0300

    tp2 centers added

--- src/python/TP2/tp2.py
+++ src/python/TP2/tp2.py
@@ -1,50 +1,71 @@
 import cv2
 import math
 
-# from drawing_utils import mosaic_view
-from src.python.TP2.Vehicle import Vehicle
 
-video_path = 'traffic.mp4'
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-background_subs = cv2.createBackgroundSubtractorMOG2(detectShadows=False, history=500, varThreshold=25)
+def tp2():
+    # cantidad de metros equivalentes a un pixel
+    meter_x_pixel = 0.026
+    # cantidad de segundos equivalentes a un frame
+    seconds_x_frame = 0.04
 
-cap = cv2.VideoCapture(video_path)
+    # definimos para saber cada cuanto medir la distancia
+    frame_counter = 0
 
-while (cap.isOpened):
-    ret, frame = cap.read()
-    clean_frame = frame.copy()
+    trackers = cv2.MultiTracker_create()
 
-    # OPERACIONES MORFOLOGICAS
-    # apply background substraction
-    detected_motion = background_subs.apply(frame)
+    video_path = 'traffic.mp4'
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    background_subs = cv2.createBackgroundSubtractorMOG2(detectShadows=False, history=500, varThreshold=150)
 
-    # marcar los autos mejor
-    detected_motion = cv2.morphologyEx(detected_motion, cv2.MORPH_CLOSE, kernel)  # Dilation followed by Erosion
+    cap = cv2.VideoCapture(video_path)
 
-    # sacar ruido del fondo
-    detected_motion = cv2.morphologyEx(detected_motion, cv2.MORPH_OPEN, kernel)  # Erosion followed by dilation
+    while (cap.isOpened):
+        ret, frame = cap.read()
+        clean_frame = frame.copy()
+        frame_counter = frame_counter + 1
 
-    # buscamos contornos de parte de arriba y abajo por separado
-    (contours, _) = cv2.findContours(detected_motion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # OPERACIONES MORFOLOGICAS
+        # apply background substraction
+        detected_motion = background_subs.apply(frame)
 
-    def valid_countor(c):
-        # boundingRect es para dibujar un rectangulo aprox al rededor de la img binaria
-        (x, y, width, h) = cv2.boundingRect(c)
-        return (cv2.contourArea(c) > 700 and width < 100)
+        # marcar los autos mejor
+        detected_motion = cv2.morphologyEx(detected_motion, cv2.MORPH_CLOSE, kernel)  # Dilation followed by Erosion
 
-    filtered_contours = list(filter(valid_countor, contours))
-    # filtered_contours = contours
+        # sacar ruido del fondo
+        detected_motion = cv2.morphologyEx(detected_motion, cv2.MORPH_OPEN, kernel)  # Erosion followed by dilation
 
-    for contour in filtered_contours:
-        # get bounding box from countour
-        (x, y, w, h) = cv2.boundingRect(contour)
-        # draw bounding box
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # buscamos contornos de parte de arriba y abajo por separado
+        (contours, _) = cv2.findContours(detected_motion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow('detected_motion', detected_motion)
-    cv2.imshow('tp2', frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+        def valid_countor(c):
+            # boundingRect es para dibujar un rectangulo aprox al rededor de la img binaria
+            (x, y, width, h) = cv2.boundingRect(c)
+            return (cv2.contourArea(c) > 250 and width < 150)
 
-cap.release()
-cv2.destroyAllWindows()
+        filtered_contours = list(filter(valid_countor, contours))
+        # filtered_contours = contours
+
+        for contour in filtered_contours:
+            # get bounding box from countour
+            (x, y, w, h) = cv2.boundingRect(contour)
+            # draw bounding box
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+            moments = cv2.moments(contour)
+            centre_x = moments["m10"]/moments["m00"]
+            centre_y = moments["m01"]/moments["m00"]
+            #
+            # if ( frame_counter % 2 == 0 ):
+            #     speed = calculateSpeed(centre_x, centre_y, )
+            # else:
+
+
+        cv2.imshow('detected_motion', detected_motion)
+        cv2.imshow('tp2', frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+tp2()
