264e74af1e1e6f135e1995780459303a7d793e18
Matias <matias.s.venditti@gmail.com>
2020-11-26 23:04:01 -0300

    roi and predictions together

--- src/python/TP3/main.py
+++ src/python/TP3/main.py
@@ -3,50 +3,40 @@ import tensorflow as tf
 import cv2
 
 
-def tp3():
-    model_path = "./saved_model/classifier"
-    image_path = "./without_mask/1.png"
-    # model = tf.saved_model.load(model_path)
-    model = tf.keras.models.load_model(model_path)
-
-
-    # image = cv2.imread(image_path)
-
-    img_height = 180
-    img_width = 180
-
-    img = tf.keras.preprocessing.image.load_img(
-        image_path, target_size=(img_height, img_width)
-    )
-
-
-    img_array = tf.keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create a batch
-
-
-    predictions = model.predict(img_array/255.)
-    score = tf.nn.softmax(predictions[0])
-    print(score)
-
-    class_names = ['with_mask', 'without_mask']
-    print("This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score)))
-
-
 def face_classifier():
+    model_path = "./saved_model/classifier"
+    model = tf.keras.models.load_model(model_path)
+    class_names = ['with_mask', 'without_mask']
+
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    img = cv2.imread('./friends.jpeg')
+    img = cv2.imread('./without_mask/1.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0))
+    for i, (x, y, w, h) in enumerate(faces):
+        roi = img[y:y+h, x:x+w]
+        resized = cv2.resize(roi, (180, 180))
+        normalized = resized/255.
+        reshaped = np.reshape(normalized, (1, 180, 180, 3))
+
+        predictions = model.predict(reshaped)
+        score = tf.nn.softmax(predictions[0])
+        label = np.argmax(score)
+
+        # Red color for no mask
+        color = (0, 0, 255)
+        if label == 0:
+            # Green color for mask
+            color = (0, 255, 0)
+
+        cv2.putText(img, class_names[label], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 1, cv2.LINE_AA)
+        cv2.rectangle(img, (x, y), (x+w, y+h), color)
 
     cv2.imshow('img', img)
     cv2.waitKey()
 
 
 if __name__ == '__main__':
-    # tp3()
     face_classifier()
