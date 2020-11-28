b863a6b153976ce0b2d4694ae431bb8634a3249e
Matias <matias.s.venditti@gmail.com>
2020-11-28 15:25:18 -0300

    working model with new images

--- src/python/TP3/main.py
+++ src/python/TP3/main.py
@@ -2,24 +2,19 @@ import numpy as np
 import tensorflow as tf
 import cv2
 
+class_names = ['with_mask', 'without_mask']
 
-def face_classifier():
-    model_path = "./saved_model/classifier"
-    model = tf.keras.models.load_model(model_path)
-    class_names = ['with_mask', 'without_mask']
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-    img = cv2.imread('./with_mask/1.png')
+def detect_faces_and_classify(img, face_cascade, model):
+    global class_names
+    faces = face_cascade.detectMultiScale(img, 1.1, 4)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
     for i, (x, y, w, h) in enumerate(faces):
-        roi = gray[y:y+h, x:x+w]
+        roi = gray[y:y + h, x:x + w]
         resized = cv2.resize(roi, (180, 180))
-        normalized = resized/255.
-        reshaped = np.reshape(normalized, (1, 180, 180, 1))
+        # normalized = resized/255.
+        reshaped = np.reshape(resized, (1, 180, 180, 1))
 
         predictions = model.predict(reshaped)
         score = tf.nn.softmax(predictions[0])
@@ -32,9 +27,22 @@ def face_classifier():
             color = (0, 255, 0)
 
         cv2.putText(img, class_names[label], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 1, cv2.LINE_AA)
-        cv2.rectangle(img, (x, y), (x+w, y+h), color)
+        cv2.rectangle(img, (x, y), (x + w, y + h), color)
 
-    cv2.imshow('img', img)
+
+def face_classifier():
+    model_path = "./saved_model/classifier"
+    model = tf.keras.models.load_model(model_path)
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    img = cv2.imread('./with_mask/1.png')
+    img2 = cv2.imread('./without_mask/1.png')
+
+    detect_faces_and_classify(img, face_cascade, model)
+    detect_faces_and_classify(img2, face_cascade, model)
+
+    cv2.imshow('With Mask', img)
+    cv2.imshow('Without Mask', img2)
     cv2.waitKey()
 
 
